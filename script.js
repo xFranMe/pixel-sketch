a13f893936405bea7f3eec6f342732fdc2ae494b
Fran Mesa <120692857+xFranMe@users.noreply.github.com>
2024-12-28 20:46:38 +0100

    Add pixel painting mode

--- script.js
+++ script.js
@@ -2,7 +2,33 @@
 const canvas = document.querySelector(".canvas");
 const rangeInputGrid = document.querySelector("#grid-dimension");
 const spanGrid = document.querySelector("span");
-const btnGrid = document.querySelector("#btn-grid-dimension")
+const btnGrid = document.querySelector("#btn-grid-dimension");
+const modes = document.querySelectorAll("input[name = 'mode']");
+const selectedColor = document.querySelector("#selected-color");
+const erase = document.querySelector("#eraser");
+let selectedMode = null;
+
+
+modes.forEach(mode => {
+    if(mode.checked) {
+        selectedMode = mode.value;
+    }
+
+    mode.addEventListener("change", () => {
+        let result = confirm("Do you want to switch mode? Your progress will be lost.");
+        if(result) { 
+            selectedMode = mode.value;
+            createNewGrid();
+        }
+        else { 
+            modes.forEach(mode => {
+                if(mode.value === selectedMode) {
+                    mode.checked = true;
+                }
+            });
+        }
+    });
+});
 
 rangeInputGrid.addEventListener("input", () => {
     spanGrid.textContent = rangeInputGrid.value; 
@@ -28,12 +54,14 @@ let isPainting = false;
 canvas.addEventListener("mousedown", (event) => {
     event.preventDefault();
     isPainting = true;
-    paintBlackTile(event);
+    if(erase.checked) { eraseTile(event) }
+    else {paintTile(event);}
 });
 
 canvas.addEventListener("mouseover", (event) => {
     if(isPainting) {
-        paintBlackTile(event);
+        if(erase.checked) { eraseTile(event) }
+        else {paintTile(event);}
     }
 });
 
@@ -41,15 +69,36 @@ canvas.addEventListener("mouseup", () => {
     isPainting = false;
 })
 
-function paintBlackTile(event) {
+function paintSketchTile(event) {
     let target = event.target;
     if(target.style.opacity < 1) {
         target.style.backgroundColor = "black";
-        event.target.style.opacity = +target.style.opacity + 0.1;
+        target.style.opacity = +target.style.opacity + 0.1;
     }
 }
 
-createNewGrid();
+function paintPixelArtTile(event) {
+    let target = event.target;
+    target.style.backgroundColor = `${selectedColor.value}`;
+}
+
+function paintTile(event) {
+    switch(selectedMode) {
+        case "sketch":
+            paintSketchTile(event);
+            break;
+        case "pixel-art":
+            paintPixelArtTile(event);
+            break;
+        default:
+    }
+}
+
+function eraseTile(event) {
+    let target = event.target;
+    target.style.removeProperty("background-color");
+    if(target.style.opacity < 1) {target.style.opacity = '';}
+}
 
 // Download
 const btnPicDownload = document.querySelector("#btn-pic-download");
@@ -62,3 +111,5 @@ btnPicDownload.addEventListener("click", () => {
         link.click();
     });
 });
+
+createNewGrid();
